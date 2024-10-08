af9859f352ffc943588dd6d2e27c22dd19cd9a6d
Jorge Eduardo Cobo Ocampo <jorge.cobo01@uceva.edu.co>
2024-10-07 21:09:20 -0500

    Organice la base de datos, repare algunos errores e hice el crud de Crear-programa

--- src/main/java/co/edu/uceva/programaservice/model/service/IProgramaService.java
+++ src/main/java/co/edu/uceva/programaservice/model/service/IProgramaService.java
@@ -1,10 +1,12 @@
 package co.edu.uceva.programaservice.model.service;
 
 
+import co.edu.uceva.programaservice.model.entities.Programa;
+
 /**
  * Esta interfaz define los metodos que se van a implementar en la clase ProgramaServiceImpl
  */
 
 public interface IProgramaService {
-
+    Programa save(Programa programa);//Guarda un programa y me retorna un objeto de tipo Programa
 }
